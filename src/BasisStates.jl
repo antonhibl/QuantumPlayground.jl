6f48bcdef0ceba7734ce2f649bc01645d63183ce
y0ka1 <antonhibl11@gmail.com>
2021-10-07 23:06:02 -0700

    adding polar plotting and conversion functions
    adding support for plotting polar forms of complex numbers as well
    as ensuring that the conversion functions to move between the 2 forms
    of complex numbers are ready. TODO: adjust polar plots to allow for
    multiple vectors on one plot.

--- src/BasisStates.jl
+++ src/BasisStates.jl
@@ -2,82 +2,80 @@
 
 # Zero Basis State
 """
-    zero_state = [1;0]
+    zero = [1;0]
 
-|0⟩ basis state as defined in terms of Quantum Computational Linear Algebra.
+|0⟩ basis state for a qubit of information.
 
 # Examples
 ```julia-repl
-julia> zero_state
+julia> zero
 [1;
  0]
  ```
 """
-zero_state = [1;0]
+zero = [1;0]
 
 # One Basis State
 """
-    one_state = [0;1]
+    one = [0;1]
 
-|1⟩ basis state as defined in terms of Quantum Computational Linear Algebra.
+|1⟩ basis state for a qubit of information.
 
 # Examples
 ```julia-repl
-julia> one_state
+julia> one
 [0;
  1]
  ```
 """
-one_state = [0;1]
+one = [0;1]
 
 
 # Defining directional basis states
 
 # Up State
 """
-    up_state = zero_state
+    up = zero
 
 Equivalent to the polarization of a bit pointing up.
 """
-up_state = zero_state
+up = zero
 # Down State
 """
-    down_state = one_state
+    down = one
 
 Equivalent to the polarization of a bit pointing down.
 """
-down_state = one_state
+down = one
 
 # Left State
 """
-    left_state = [1/sqrt(2);1/sqrt(2)]
+    left = [1/√(2);1/√(2)]
 
 Equivalent to the polarization of a bit pointing to the left.
 """
-left_state = [1/sqrt(2);1/sqrt(2)]
+left = [1/√(2);1/√(2)]
 
 # Right State
 """
-    right_state = [1/sqrt(2);-1/sqrt(2)]
+    right = [1/√(2);-1/√(2)]
 
 Equivalent to the polarization of a bit pointing to the right.
 """
-right_state = [1/sqrt(2);-1/sqrt(2)]
+right = [1/√(2);-1/√(2)]
 
 # Up-right State
 """
-    upright_state = [1/2;-sqrt(3)/2]
+    upright = [1/2;-√(3)/2]
 
 Equivalent to the polarization of a bit pointing up to the right.
 """
-upright_state = [1/2;-sqrt(3)/2] 
+upright = [1/2;-√(3)/2] 
 
 # Down-left State
 """
-    downleft_state = [sqrt(3)/2;1/2]
+    downleft = [√(3)/2;1/2]
 
 Equivalent to the polarization of a bit pointing down to the left.
 """
-downleft_state = [sqrt(3)/2;1/2]
-
-#
+downleft = [√(3)/2;1/2]
--- src/QuantumPlayground.jl
+++ src/QuantumPlayground.jl
@@ -5,9 +5,11 @@ import LinearAlgebra
 import Calculus
 import Statistics
 import MathPhysicalConstants
+import Plots
 using Calculus
 using Statistics
 using MathPhysicalConstants
+using Plots
 
 # Euler's Constant
 """
@@ -21,7 +23,8 @@ e = MathConstants.e # Euler's Constant
 """
     × = LinearAlgebra.kron
 
-written as \times in julia, this allows two matrixes to be combined into a tensor product to be used in larger circuits with more bits.
+written as \times in julia, this allows two matrixes to be combined 
+into a tensor product to be used in larger circuits with more bits.
 
 # Examples
 ```julia-repl
@@ -35,28 +38,69 @@ julia> I×I
 
 # Polar Form Conversion Operation
 """
-    polarform(complex_number) = r*(cos(θ)+im*sin(θ))
+    cartesian2polar(cartesian_complex_number) --> (p, θ)
 
-A conversion function which can convert a complex number into its equivalent polar form. Can also be applied iteratively to matrices.
+A conversion function which takes a cartesian form of a complex number, 
+a+bi, and transforms it into its polar form, (p, θ).
 
 # Examples
 ```julia-repl
-julia> polarform(2-3i)
-3.605551275463989 * cos(-0.982793723247329) + im * sin(-0.982793723247329)
+julia> cartesian2polar(2-3i)
+(3.605551275463989, -0.982793723247329)
 
-2.0-2.99999im
 ```
 """
-function polarform(complex_number)
-	a = complex_number.re
-	b = complex_number.im
-	r = √(((a)^2)+((b)^2))
+function cartesian2polar(cartesian_complex_number)
+	a = cartesian_complex_number.re
+	b = cartesian_complex_number.im
+	p = √((a^2)+(b^2))
 	θ = atan(b/a)
-	polar = r*(cos(θ)+im*sin(θ))
-	print(r, " ", *, " ", cos, "(", θ, ")", " ", +, " ", im, " ", *, " ", sin, "(", θ, ")\n\n")
+
+	polar = ( p, θ )
+
 	return polar
 end
-		
+
+# Polar Form to Cartesian form function
+"""
+    polar2cartesian(polar_complex_number) --> a=bi
+
+A conversion function which takes a polar form of a complex number, (p, θ), and transforms it into
+it's cartesian form.
+
+# Examples
+```julia-repl
+julia> polar2cartesian()
+```
+"""
+function polar2cartesian(polar_complex_number)
+	p = polar_complex_number[1]
+	θ = polar_complex_number[2]
+
+	complex = (p*(cos(θ)+im*sin(θ)))
+
+	if complex == (p*e^(im*θ))
+		return complex
+	else 
+		print("Error")
+	end
+end
+
+Plots.plotly()
+
+# Polar Form Plotting Function
+"""
+    polarplot(polar_complex_number)
+
+Plots a given polar form number on the complex polar plane.
+"""
+function polarplot(polar_complex_number)
+	p = polar_complex_number[1]
+	θ = polar_complex_number[2]
+        
+	plot([0, θ], [0, p], proj = :polar, m = 2)
+end
+	
 # Inclusions
 include("QubitGates.jl")
 include("PhotonPlayground.jl")
